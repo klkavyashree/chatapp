0a88e28db82270b1a798d776c00e721e0a201152
klkavyashree <kavya.kavyashree97@gmail.com>
2019-02-15 18:23:40 +0530

    chat

--- server/server.js
+++ server/server.js
@@ -56,25 +56,3 @@ server.listen(port, () => {
 
     console.log(`Example app listening on port ${port}!`)
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-// mongoose.Promise = global.Promise;//return native code 
